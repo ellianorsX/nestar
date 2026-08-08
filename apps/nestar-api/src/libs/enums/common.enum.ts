a615e881a87c25a0aea045ea498aea5e23c16db2
Marjona Rakhmonova <marjonathe98@icloud.com>
2026-08-08 11:56:22 +0900

    feat: build project standards

---
+++ apps/nestar-api/src/libs/enums/common.enum.ts
@@ -0,0 +1,19 @@
+export enum Message {
+	SOMETHING_WENT_WRONG = 'Something went wrong',
+	NO_DATA_FOUND = 'No data found',
+	CREATE_FAILED = 'Failed to create',
+	UPDATE_FAILED = 'Failed to update',
+	REMOVE_FAILED = 'Failed to remove',
+	UPLOAD_FAILED = 'Failed to upload',
+	BAD_REQUEST = 'Bad request',
+
+	NO_MEMBER_NICK = 'No member with that member nick!',
+	BLOCKED_USER = 'You have been blocked!',
+	WRONG_PASSWORD = 'Wrong password, try again!',
+	NOT_AUTHENTICATED = 'You are not authenticated, please login first!',
+	TOKEN_NOT_EXIST = 'Bearer Token is not provided!',
+	ONLY_SPECIFIC_ROLES_ALLOWED = 'Allowed only for members with specific roles!',
+	NOT_ALLOWED_REQUEST = 'Not Allowed Request!',
+	PROVIDE_ALLOWED_FORMAT = 'Please provide jpg, jpeg or png images!',
+	SELF_SUBSCRIPTION_DENIED = 'Self subscription is denied!',
+}
