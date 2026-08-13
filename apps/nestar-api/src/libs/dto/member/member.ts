9c6bee7f7a856fcc8cc7d4c0bd58aeef3e7e7ff5
Marjona Rakhmonova <marjonathe98@icloud.com>
2026-08-13 15:06:30 +0900

    feat: develop signup bussiness logic

---
+++ apps/nestar-api/src/libs/dto/member/member.ts
@@ -0,0 +1,80 @@
+import { Field, Int, ObjectType } from '@nestjs/graphql';
+import { MemberAuthType, MemberStatus, MemberType } from '../../enums/member.enum';
+import { ObjectId } from 'mongoose';
+
+@ObjectType()
+export class Member {
+	@Field(() => String)
+	_id: ObjectId | undefined;
+
+	@Field(() => MemberType)
+	memberType: MemberType | undefined;
+
+	@Field(() => MemberStatus)
+	memberStatus: MemberStatus | undefined;
+
+	@Field(() => MemberAuthType)
+	memberAuthType: MemberAuthType | undefined;
+
+	@Field(() => String)
+	memberPhone: string | undefined;
+
+	@Field(() => String)
+	memberNick: string | undefined;
+
+	memberPassword?: string | undefined;
+
+	@Field(() => String, { nullable: true })
+	memberFullName?: string | undefined;
+
+	@Field(() => String, { nullable: true })
+	memberImage?: string | undefined;
+
+	@Field(() => String, { nullable: true })
+	memberAddress?: string | undefined;
+
+	@Field(() => String, { nullable: true })
+	memberDesc?: string | undefined;
+
+	@Field(() => Int)
+	memberProperties: number | undefined;
+
+	@Field(() => Int)
+	memberArticles: number | undefined;
+
+	@Field(() => Int)
+	memberFollowers: number | undefined;
+
+	@Field(() => Int)
+	memberFollowings: number | undefined;
+
+	@Field(() => Int)
+	memberPoints: number | undefined;
+
+	@Field(() => Int)
+	memberLikes: number | undefined;
+
+	@Field(() => Int)
+	memberViews: number | undefined;
+
+	@Field(() => Int)
+	memberComments: number | undefined;
+
+	@Field(() => Int)
+	memberRank: number | undefined;
+
+	@Field(() => Int)
+	memberWarnings: number | undefined;
+
+	@Field(() => Int)
+	memberBlocks: number | undefined;
+
+	@Field(() => Date, { nullable: true })
+	deletedAt?: Date;
+
+	@Field(() => Date, { nullable: true })
+	createdAt?: Date;
+
+	@Field(() => Date)
+	updatedAt?: Date;
+}
