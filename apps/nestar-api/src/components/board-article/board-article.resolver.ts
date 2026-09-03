bc3144ed91277426fb695ffc6cc0f626de89caee
Marjona Rakhmonova <marjonathe98@icloud.com>
2026-09-03 15:05:41 +0900

    feat: develop boardArticle relate GraphQL APIs

--- apps/nestar-api/src/components/board-article/board-article.resolver.ts
+++ apps/nestar-api/src/components/board-article/board-article.resolver.ts
@@ -1,7 +1,71 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { BoardArticleService } from './board-article.service';
+import { UseGuards } from '@nestjs/common';
+import { AuthGuard } from '../auth/guards/auth.guard';
+import { BoardArticle, BoardArticles } from '../../libs/dto/board-article/board-article';
+import { AuthMember } from '../auth/decorators/authMember.decorator';
+import { BoardArticleInput, BoardArticlesInquiry } from '../../libs/dto/board-article/board-article.input';
+import * as mongoose from 'mongoose';
+import { WithoutGuard } from '../auth/guards/without.guard';
+import { shapeIntoMongoObjectId } from '../../libs/config';
+import { BoardArticleUpdate } from '../../libs/dto/board-article/board-article.update';
 
 @Resolver()
 export class BoardArticleResolver {
 	constructor(private readonly boardArticleService: BoardArticleService) {}
+
+	/**''''''''''''''''''''''''''''createBoardArticles''''''''''''''''''''''''''''''''''''''''''''  **/
+	@UseGuards(AuthGuard)
+	@Mutation(() => BoardArticle)
+	public async createBoardArticle(
+		@Args('input') input: BoardArticleInput,
+		@AuthMember('_id') memberId: mongoose.ObjectId,
+	): Promise<BoardArticle> {
+		console.log('Mutation: createBoardArticle');
+
+		return await this.boardArticleService.createBoardArticle(memberId, input);
+	}
+
+	/**''''''''''''''''''''''''''''getBoardArticle''''''''''''''''''''''''''''''''''''''''''''  **/
+
+	@UseGuards(WithoutGuard)
+	@Query(() => BoardArticle)
+	public async getBoardArticle(
+		@Args('articleId') input: string,
+		@AuthMember('_id') memberId: mongoose.ObjectId,
+	): Promise<BoardArticle> {
+		console.log('Query: getProperty');
+
+		const articleId = shapeIntoMongoObjectId(input);
+
+		return await this.boardArticleService.getBoardArticle(memberId, articleId);
+	}
+
+	/**''''''''''''''''''''''''''''updateBoardArticles''''''''''''''''''''''''''''''''''''''''''''  **/
+	@UseGuards(AuthGuard)
+	@Mutation(() => BoardArticle)
+	public async updateBoardArticle(
+		@Args('input') input: BoardArticleUpdate,
+		@AuthMember('_id') memberId: mongoose.ObjectId,
+	): Promise<BoardArticle> {
+		console.log('Mutation: updateBoardArticle');
+
+		input._id = shapeIntoMongoObjectId(input._id);
+
+		return await this.boardArticleService.updateBoardArticle(memberId, input);
+	}
+
+	/**''''''''''''''''''''''''''''getBoardArticles''''''''''''''''''''''''''''''''''''''''''''  **/
+	@UseGuards(WithoutGuard)
+	@Query(() => BoardArticles)
+	public async getBoardArticles(
+		@Args('input') input: BoardArticlesInquiry,
+		@AuthMember('_id') memberId: mongoose.ObjectId,
+	): Promise<BoardArticles> {
+		console.log('Query: getBoardArticles');
+
+		// input.search.memberId = shapeIntoMongoObjectId(input.search.memberId);
+
+		return await this.boardArticleService.getBoardArticles(memberId, input);
+	}
 }
