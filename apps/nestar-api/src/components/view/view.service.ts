35f5675a718f05c7d08840f785de04f6dc521542
Marjona Rakhmonova <marjonathe98@icloud.com>
2026-09-08 09:24:54 +0900

    feat: develop getVisited GraphQL API

--- apps/nestar-api/src/components/view/view.service.ts
+++ apps/nestar-api/src/components/view/view.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, ObjectId } from 'mongoose';
 
 import { T } from '../../libs/types/common';
 import { ViewInput } from '../../libs/dto/member/view/view.input';
 import { View } from '../../libs/dto/member/view/view';
+import { OrdinaryInquiry } from '../../libs/dto/property/property.input';
+import { ViewGroup } from '../../libs/enums/view.enum';
+import { Properties } from '../../libs/dto/property/property';
+import { lookupVisit } from '../../libs/config';
 
 @Injectable()
 export class ViewService {
@@ -22,5 +26,42 @@ export class ViewService {
 		const { memberId, viewRefId } = input;
 		const search: T = { memberId: memberId, viewRefId: viewRefId };
 		return await this.viewModel.findOne(search).exec();
+	}
+
+	public async getVisitedProperties(memberId: ObjectId, input: OrdinaryInquiry): Promise<Properties> {
+		const { page, limit } = input;
+		const match: T = { likeGroup: ViewGroup.PROPERTY, memberId: memberId };
+
+		const data: T = await this.viewModel
+			.aggregate([
+				{ $match: match },
+				{ $sort: { updatedAt: -1 } },
+				{
+					$lookup: {
+						from: 'properties',
+						localField: 'viewRefId',
+						foreignField: '_id',
+						as: 'visitedProperty',
+					},
+				},
+				{ $unwind: '$visitedProperty' },
+				{
+					$facet: {
+						list: [
+							{ $skip: (page - 1) * limit },
+							{ $limit: limit },
+							lookupVisit,
+							{ $unwind: '$visitedProperty.memberData' },
+						],
+						metaCounter: [{ $count: 'total' }],
+					},
+				},
+			])
+			.exec();
+
+		const result: Properties = { list: [], metaCounter: data[0].metaCounter };
+		result.list = data[0].list.map((ele) => ele.visitedProperty);
+		console.log('result:', result);
+		return result;
 	}
 }
