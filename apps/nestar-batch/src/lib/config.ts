0de88a21f1cef4025dcea5d6249c596fc6547030
Marjona Rakhmonova <marjonathe98@icloud.com>
2026-09-10 17:16:51 +0900

    feat: create batchRollBack, batchTopProperties, bachtTopAgents jobScgedulers logic

---
+++ apps/nestar-batch/src/lib/config.ts
@@ -0,0 +1,7 @@
+/************************************************************************************
+ *                                  BATCH CONSTANTS
+ *************************************************************************************/
+
+export const BATCH_ROLLBACK = 'ROLLBACK';
+export const BATCH_TOP_AGENTS = 'TOP_AGENTS';
+export const BATCH_TOP_PROPERTIES = 'BATCH_TOP_PROPERTIES';
--- apps/nestar-batch/src/nestar-batch.controller.ts
+++ apps/nestar-batch/src/nestar-batch.controller.ts
@@ -1,12 +1,58 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 import { NestarBatchService } from './nestar-batch.service';
+import { Cron, Interval, Timeout } from '@nestjs/schedule';
 
 @Controller()
 export class NestarBatchController {
-  constructor(private readonly nestarBatchService: NestarBatchService) {}
+	[x: string]: any;
+	private readonly logger = new Logger(NestarBatchController.name);
+	constructor(private readonly nestarBatchService: NestarBatchService) {}
 
-  @Get()
-  getHello(): string {
-    return this.nestarBatchService.getHello();
-  }
+	@Timeout(1000)
+	handleTimeout() {
+		this.logger.debug('Batch Service Started');
+	}
+
+	@Cron('00 * * * * *', { name: 'BATCH_ROLLBACK' })
+	public async batchRollback() {
+		try {
+			this.logger['context'] = 'BATCH_ROLLBACK';
+			this.logger.debug('EXECUTED!');
+			await this.nestarBatchService.batchRollback();
+		} catch (err) {
+			this.logger.error(err);
+		}
+	}
+
+	@Cron('20 * * * * *', { name: 'BATCH_TOP_PROPERTIES' })
+	public async batchTopProperties() {
+		try {
+			this.logger['context'] = 'BATCH_TOP_PROPERTIES';
+			this.logger.debug('EXECUTED!');
+			await this.nestarBatchService.batchTopProperties();
+		} catch (err) {
+			this.logger.error(err);
+		}
+	}
+
+	@Cron('40 * * * * *', { name: 'BATCH_TOP_AGENTS' })
+	public async batchTopAgents() {
+		try {
+			this.logger['context'] = 'BATCH_TOP_AGENTS';
+			this.logger.debug('EXECUTED!');
+			await this.nestarBatchService.batchTopAgents();
+		} catch (err) {
+			this.logger.error(err);
+		}
+	}
+
+	/*@Interval(10000)
+	handleInterval() {
+		this.logger.debug('INTERNAL TEST');
+	}*/
+
+	@Get()
+	getHello(): string {
+		return this.nestarBatchService.getHello();
+	}
 }
